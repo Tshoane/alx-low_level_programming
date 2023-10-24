5c26684a0e5bdb10b1f1ac027be25442703de842
Tshoane <respectmasehla@icloud.com>
2023-10-24 07:41:04 +0200

    updated

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,50 +1,20 @@
-#include "lists.h"
 
-/**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
- */
-size_t listint_len(const listint_t *h)
-{
- size_t num = 0;
-
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
-}
+#include <stdlib.h>
+	#include "dog.h"
 
 
-=============================
-
-2-add_nodeint.c
-
-#include "lists.h"
-
-/**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
- *
- * Return: pointer to the new node, or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
- listint_t *new;
-
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
-
- new->n = n;
- new->next = *head;
- *head = new;
-
- return (new);
-}
-
+	/**
+	 * init_dog - initializes a variable of type struct dog
+	 * @d: pointer to struct dog to initialize
+	 * @name: name to initialize
+	 * @age: age to initialize
+	 * @owner: owner to initialize
+	 */
+	void init_dog(struct dog *d, char *name, float age, char *owner)
+	{
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
