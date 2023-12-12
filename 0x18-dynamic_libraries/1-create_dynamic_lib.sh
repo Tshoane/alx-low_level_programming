16877aa545cb283c148a6836aa3995590d0cf0a1
tshoane <respectmasehla@icloud.com>
2023-12-11 19:10:29 -0800

    updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,10 @@
-#!/bim/bash
-gcc -wall -wextra -werror -pedantic -c -FPIC *.c
+#!/bin/bash
+
+# Compile all .c files into object files
+gcc -c -Wall -Werror -fpic *.c
+
+# Create the dynamic library liball.so from the object files
 gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH.:$LD_LIBRARY_PATH
+
+# Clean up the object files
+rm *.o
